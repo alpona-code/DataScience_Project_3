c96641257bef2dae388a9ad660a60aba420194ff
alpona-code <mailto.alpona916@gmail.com>
2024-10-18 18:41:20 +0100

    updated version

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Function to call Google Gemini Pro Vision API and get the response
 def get_gemini_response(input, image, prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel('gemini-1.5-flash')
     response = model.generate_content([input, image[0], prompt])
     return response.text
 
